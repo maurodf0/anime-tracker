e9a254a7ca8681a427b5a3b569b62e8ccf79d6e8
Mauro <maurodefalco96@gmail.com>
2024-11-17 20:37:47 +0100

    type error fix

--- app.vue
+++ app.vue
@@ -62,7 +62,7 @@
     <h1 class="text-4xl my-5">AnimeTracker</h1>
 
 <UForm @submit="searchAnime()">
- <UFormGroup help="Search for you anime">
+ <UFormGroup help="Search for your anime">
    <UInput v-model="query" placeholder="Naruto"></UInput>
  </UFormGroup>
  <UButton variant="solid" :loading="pending">Search</UButton>
