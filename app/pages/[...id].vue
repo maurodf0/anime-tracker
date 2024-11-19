ebe0cb276ff2e434ffd360bf1a8dfbb2a15d971e
Mauro <maurodefalco96@gmail.com>
2024-11-20 00:22:09 +0100

    fetch single item data

--- app/pages/[...id].vue
+++ app/pages/[...id].vue
@@ -8,6 +8,53 @@
 <script setup>
 
 const route = useRoute()
+const id = route.params.id;
 
-console.log(route.params.id);
+const url = `https://api.jikan.moe/v4/anime/${id}`;
+
+onMounted( () => {
+    const {data, error} = await useFetch(url);
+    if(error){
+        toast.add({
+        title: 'Error retriving data',
+        description: 'Write your anime before start searching',
+        color: 'red'
+      })
+    }
+}) 
+
+
+const searchAnime = async (number = 1) => {
+    if (!query.value){
+      toast.add({
+        title: 'You must insert data for search',
+        description: 'Write your anime before start searching',
+        color: 'red'
+      })
+    }; 
+
+    pending.value = true;
+    const url = `https://api.jikan.moe/v4/anime?q=${query.value}&page=${number}`;
+    try {
+    const {data, error} = await useFetch(url);
+    if(error.value){
+      toast.add({
+        title: 'Error in fetching data',
+        description: error.value,
+        color: 'red'
+      })
+    } else {
+      searchResult.value = data.value.data;
+      pagination.value = data.value.pagination;
+      console.log(searchResult.value);
+      console.log(pagination.value);
+    }
+
+    } catch(error) {
+      console.error('Errore durante la ricerca:', error);
+    } finally {
+      pending.value = false;
+     
+    }
+  }
 </script>
