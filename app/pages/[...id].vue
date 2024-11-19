9fc5a281f56adb7bc82a98f84a64c679d9ba81b5
Mauro <maurodefalco96@gmail.com>
2024-11-20 00:16:25 +0100

    routing test

--- app/pages/[...id].vue
+++ app/pages/[...id].vue
@@ -1,9 +1,13 @@
 <template>
     <article>
         <h1>test</h1>
+        {{ route.params.id }}
     </article>
 </template>
+
 <script setup>
 
-    
+const route = useRoute()
+
+console.log(route.params.id);
 </script>
