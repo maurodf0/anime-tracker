7d0fa3030a4386f3de5bfa4da902275d1bd2386c
Mauro <maurodefalco96@gmail.com>
2024-11-20 00:52:36 +0100

    layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <UContainer class="max-w-[1080px] mx-auto px-4">
+        <<h1 class="text-4xl my-5">AnimeTracker</h1>
+
+        <UForm @submit="searchAnime()">
+        <UFormGroup help="Search for your anime">
+        <UInput v-model="query" placeholder="Naruto"></UInput>
+        </UFormGroup>
+        <UButton variant="solid" :loading="pending">Search</UButton>
+        </UForm>
+
+        <slot />
+    </UContainer>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -57,15 +57,7 @@
 </script>
 
 <template>
-    <UContainer class="max-w-[1080px] mx-auto px-4">
-    <h1 class="text-4xl my-5">AnimeTracker</h1>
 
-<UForm @submit="searchAnime()">
- <UFormGroup help="Search for your anime">
-   <UInput v-model="query" placeholder="Naruto"></UInput>
- </UFormGroup>
- <UButton variant="solid" :loading="pending">Search</UButton>
-</UForm>
 
 
    <h2 class="text-5xl font-bold my-4">Anime List</h2>
@@ -111,5 +103,5 @@
 
 
   <UNotifications />
-</UContainer>
+
 </template>
