f46df1bb9d3e38411afe8377c319a11df19aa3f1
Mauro <maurodefalco96@gmail.com>
2024-11-21 00:44:34 +0100

    fix footer

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="flex justify-center py-5 w-12 background-blue-800">
+    <footer class="flex w-100 w-full justify-center py-5 w-12 background-blue-800">
         <h4>AnimeTracker</h4>
     </footer>
 </template>
