b57e35fd85e1bc42e1426519fb9746a9c6753e45
Mauro <maurodefalco96@gmail.com>
2024-11-22 17:50:10 +0100

    track Btn

--- app/pages/[...id].vue
+++ app/pages/[...id].vue
@@ -19,6 +19,7 @@
             <h2 class="text-2xl mb-4">Trailer</h2>
             <iframe width="560" height="315" :src="anime?.trailer?.embed_url" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </article>
+        <UButton variant="solid">Start Tracking</UButton>
     </UContainer>
 </template>
 
