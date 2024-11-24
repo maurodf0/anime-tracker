01f36f2c6a51f278f5de2088efdaa48b896a2f0d
Mauro <maurodefalco96@gmail.com>
2024-11-24 23:53:24 +0100

    animecard

--- app/pages/my-list.vue
+++ app/pages/my-list.vue
@@ -1,7 +1,18 @@
 <template>
-    <ul>
-        <li v-for="anime in myAnimeList" :key="anime.id">{{ anime.title }}</li>
-    </ul>
+    <UContainer class="max-w-[1080px] mx-auto px-4 min-h-screen">
+        <div 
+            v-for="anime in myAnimeList" :key="anime.id"
+            class="flex justify-between border border-gray-700 rounded p-4">
+            <div class="anime-info">
+           <h4> {{ anime.title }}</h4>
+           <img :src="anime.img">
+           <div class="counter">
+            +
+            -
+           </div>
+            </div>
+        </div>
+    </UContainer>
 </template>
 
 <script setup>
