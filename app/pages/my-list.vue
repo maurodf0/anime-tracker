a00529bd6c6dfa6e698ad09b88928a86bf393d35
Mauro <maurodefalco96@gmail.com>
2024-11-24 23:47:05 +0100

    tracked anime

---
+++ app/pages/my-list.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul>
+        <li v-for="anime in storedList" :key="anime.id">{{ anime.title }}</li>
+    </ul>
+</template>
+
+<script setup>
+
+onMounted(() => {
+    const storedList = localStorage.getItem('animeStorage');
+    if (storedList) {
+        myAnimeList.value = JSON.parse(storedList);
+    }
+});
+
+</script>
