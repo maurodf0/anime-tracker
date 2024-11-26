697f3d469a37775462bb24a2cf2d843e3cca4217
Mauro <maurodefalco96@gmail.com>
2024-11-26 10:30:46 +0100

    fix url

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <UContainer class="max-w-[1080px] mx-auto px-4 flex justify-between align-middle items-center">
-            <NuxtLink to="http://localhost:3000/"><h1 class="text-4xl my-5">AnimeTracker</h1></NuxtLink>
+            <NuxtLink to="/"><h1 class="text-4xl my-5">AnimeTracker</h1></NuxtLink>
             <ul>
                 <li><NuxtLink to="/my-list">My Anime</NuxtLink></li>
             </ul>
