4b3435adbcbcdf4cbcd9bc25948f2d9b9bcea550
Mauro <maurodefalco96@gmail.com>
2024-11-28 00:30:57 +0100

    change link to https://anime-tracker-delta.vercel.app/

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,9 +1,9 @@
 <template>
     <header>
         <UContainer class="max-w-[1080px] mx-auto px-4 flex justify-between align-middle items-center">
-            <NuxtLink to="http://localhost:3000/"><h1 class="text-4xl my-5">AnimeTracker</h1></NuxtLink>
+            <NuxtLink to="https://anime-tracker-delta.vercel.app/"><h1 class="text-4xl my-5">AnimeTracker</h1></NuxtLink>
             <ul>
-                <li><NuxtLink to="http://localhost:3000/my-list/">My Anime</NuxtLink></li>
+                <li><NuxtLink to="https://anime-tracker-delta.vercel.app/my-list/">My Anime</NuxtLink></li>
             </ul>
         </UContainer>
     </header>   
