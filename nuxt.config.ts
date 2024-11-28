5a5b7b5b8e41100fb3cd82d745f353cfe39635b2
Mauro <maurodefalco96@gmail.com>
2024-11-29 00:29:29 +0100

    pwa settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,4 +9,48 @@ export default defineNuxtConfig({
     pageTransition: { name: 'page', mode: 'out-in' }
   },
   modules: ['@nuxt/ui', '@nuxtjs/tailwindcss', '@formkit/auto-animate/nuxt', '@nuxtjs/pwa']
+
+  pwa: {
+    manifest: {
+      name: 'Nome della tua app',
+      short_name: 'App',
+      description: 'Descrizione della tua app',
+      lang: 'it',  // Imposta la lingua della tua PWA
+      display: 'standalone', // Come verrà mostrata l'app (standalone = come una app nativa)
+      background_color: '#ffffff', // Colore di sfondo
+      theme_color: '#4DBA87', // Colore principale della tua app
+      icons: [
+        {
+          src: '/icon.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icon.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    },
+    workbox: {
+      runtimeCaching: [
+        {
+          urlPattern: 'https://your-api-url.com/.*',
+          handler: 'NetworkFirst',
+          method: 'GET',
+          strategyOptions: {
+            cacheName: 'api-cache'
+          }
+        },
+        {
+          urlPattern: 'https://cdn.jsdelivr.net/.*',
+          handler: 'CacheFirst',
+          method: 'GET',
+          strategyOptions: {
+            cacheName: 'cdn-cache'
+          }
+        }
+      ]
+    }
+  }
 })
