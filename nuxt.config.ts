1bb91d875bf19a58e9170d7376c340da727354c8
Mauro <maurodefalco96@gmail.com>
2024-12-01 20:06:09 +0100

    PWA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,15 +19,29 @@ export default defineNuxtConfig({
       name: 'AnimeTracker',
       short_name: 'AnimeTracker',
       description: 'AnimeTracker: The App for Track your animes, choose every anime you are watching and start tracking it',
-
+      display: 'standalone',
+      start_url: '/',
+      background_color: '#ffffff',
+      theme_color: '#000000',
+      icons: [
+        {
+          src: '/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
     },
     workbox: {
-     navigateFallback: '/',
+      navigateFallback: '/',
     },
     devOptions: {
       enabled: true,
       type: 'module',
-
     },
   }
 })
