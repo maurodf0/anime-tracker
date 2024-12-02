7ad37d89473246c49b0fb47a9cd715f708b86c16
Mauro <maurodefalco96@gmail.com>
2024-12-03 00:53:18 +0100

    Change Logo

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <UContainer class="max-w-[1080px] mx-auto px-4 flex justify-between align-middle items-center">
-            <NuxtLink to="/"><h1 class="text-4xl my-5">AnimeTracker</h1></NuxtLink>
+            <NuxtLink to="/"><h1 class="text-4xl my-5">Anime<span class="tracker">Tracker</span></h1></NuxtLink>
             <ul>
                 <li><NuxtLink to="/my-list/">My Anime</NuxtLink></li>
             </ul>
@@ -10,3 +10,10 @@
 
 
 </template>
+
+<style scoped>
+    .tracker {
+        color: #4ade80 !important;
+        font-style: italic;
+    }
+</style>
