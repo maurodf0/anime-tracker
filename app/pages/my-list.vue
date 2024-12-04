c5561de559bdaca4ce7fc4aa514094e75bb00a6b
Mauro <maurodefalco96@gmail.com>
2024-12-05 00:41:55 +0100

    css to pages

--- app/pages/my-list.vue
+++ app/pages/my-list.vue
@@ -47,3 +47,17 @@ const filteredAnime = computed(() => {
      }
 
 </style>
+
+
+
+<style scoped>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
