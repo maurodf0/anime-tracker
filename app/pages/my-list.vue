d38e9aef84350a3fc3f4dd86db537a4630dae20c
Mauro <maurodefalco96@gmail.com>
2024-12-08 13:04:09 +0100

    remove transition

--- app/pages/my-list.vue
+++ app/pages/my-list.vue
@@ -48,14 +48,3 @@ const filteredAnime = computed(() => {
 
 
 
-<style scoped>
-.page-enter-active,
-.page-leave-active {
-  transition: all 0.4s;
-}
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-
-}
-</style>
