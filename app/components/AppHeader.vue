ea5f0dc3d9ae0dcb2fbd0c1098467a620bbcdc11
Mauro <maurodefalco96@gmail.com>
2024-12-10 00:00:26 +0100

    remove aniamtion

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -22,35 +22,3 @@
     }
 </style>
 
-<script setup>
-    import { gsap } from 'gsap'
-
-    const logo = ref(null);
-
-
-    onMounted(() => {
-        const Tracker = document.querySelector('.tracker')
-        gsap.from(logo.value, {
-            duration: 1,
-            opacity: 0,
-            y: -40,
-            stagger: 0.1
-        })
-
-        gsap.fromTo(Tracker, {
-            duration: 1,
-            opacity: 0,
-            y: -40,
-            stagger: 0.1,
-            letterSpacing: '10px',
-            fontWeight: 'bold'
-        }, {
-            fontWeight: 'normal',
-            letterSpacing: '0px',
-            opacity: 1,
-            y: 0,
-        })
-    
-    })
- 
-</script>
